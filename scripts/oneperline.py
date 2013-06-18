585a89b18348f959f22006b3d908bf34c32a981c
Wes <wfeely@cs.cmu.edu>
2013-06-18 14:54:35 -0400

    adjusted script to place one word per line

--- scripts/oneperline.py
+++ scripts/oneperline.py
@@ -1,26 +1,23 @@
 #!/usr/bin/python
-#Create tagger input from plain text file
+#Place every word in text file into a new file, with one word per line
 import sys
 
 #Main function
 def main(args):
 	if len(args) < 3:
-		print 'Usage: python tagging_prep.py input output'
-	data = open(args[1],'r').readlines()
+		print "Usage: python oneperline.py input_file output_file"
+		return 1
+	data = open(args[1]).readlines()
 	f = open(args[2],'w')
-	c = 0
 	for line in data:
-    		c+=1
     		if line.isspace():
         		f.write('\n')
-        		c=0
         		continue
-    		line = line.split()
-		for word in line:
-			f.write(word + '\n')
+		for word in line.split():
+			f.write(word+'\n')
 		f.write('\n')
 	f.close()
 	return 0
 
 if __name__ == '__main__':
-    sys.exit(main(sys.argv))
+	sys.exit(main(sys.argv))
