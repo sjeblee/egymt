c29f68ed80c88b75e8786d716b4d8fe1e6b9035b
sjeblee <sjeblee@gmail.com>
2013-07-08 20:39:58 -0400

    fixed bugs in normalizer and tokenizer

--- scripts/arabicNorm.py
+++ scripts/arabicNorm.py
@@ -2,30 +2,34 @@
 # -*- coding: utf-8 -*-
 #arabicNorm.py
 #Weston Feely
-#6/26/13
+#7/8/13
 import sys, re, fileinput
 
 #Compile diacritics regex
 diacritics = ur'[\u0610\u0611\u0612\u0613\u0614\u0615\u0616\u0617\u0618\u0619\u061a\u064b\u064c\u064d\u064e\u064f\u0650\u0651\u0652\u0653\u0654\u0655\u0656\u0657\u0658\u0659\u065a\u065b\u065c\u065d\u065e\u065f\u0670\u06d6\u06d7\u06d8\u06d9\u06da\u06db\u06dc\u06df\u06e0\u06e1\u06e2\u06e3\u06e4\u06e7\u06e8\u06ea\u06eb\u06ec\u06ed]'
-norm = re.compile(diacritics)
+normaccents = re.compile(diacritics,flags=re.U)
+numerals = {ur'\u0660':ur'0',ur'\u0661':ur'1',ur'\u0662':ur'2',ur'\u0663':ur'3',ur'\u0664':ur'4',
+ur'\u0665':ur'5',ur'\u0666':ur'6',ur'\u0667':ur'7',ur'\u0668':ur'8',ur'\u0669':ur'9',
+ur'\u06f0':ur'0',ur'\u06f1':ur'1',ur'\u06f2':ur'2',ur'\u06f3':ur'3',ur'\u06f4':ur'4',
+ur'\u06f5':ur'5',ur'\u06f6':ur'6',ur'\u06f7':ur'7',ur'\u06f8':ur'8',ur'\u06f9':ur'9'}
+letters = {ur'\u0622':ur'\u0627',ur'\u0623':ur'\u0627',ur'\u0625':ur'\u0627',ur'\u0624':ur'\u0648',ur'\u0626':ur'\u064a',ur'\06c0':ur'\u0674'}
 
 #Normalize Arabic text string, by removing diacritics, replacing diacriticized alif, ha, ya, waw with standard versions
 def normArabic(arabicString):
 	#Strip whitespace, ZWNJ, and non-break space from arabicString
 	arabicString = arabicString.decode('utf-8').strip(u'\xa0\u200c \t\n\r\f\v')
 	#Remove all diacritics
-	arabicString = norm.sub(ur'',arabicString).encode('utf-8')
-	#Normalize alif
-	arabicString = re.sub('أ','ا',arabicString)
-	arabicString = re.sub('إ','ا',arabicString)
-	arabicString = re.sub('آ','ا',arabicString)
-	#Normalize ha
-	arabicString = re.sub('ۀ','ه',arabicString)
-	#Normalize ya
-	arabicString = re.sub('ي','ﻯ',arabicString)
-	arabicString = re.sub('ئ','ﻯ',arabicString)
-	#Normalize waw
-	arabicString = re.sub('ؤ','و',arabicString)
+	arabicString = normaccents.sub(ur'',arabicString)
+	#Normalize Arabic letters (robust to diacritic removal)
+	for letter in letters:
+		arabicString = re.sub(letter,letters[letter],arabicString,flags=re.U)
+	#Normalize Arabic alif maksura (alif -> alif maksura at end of word)
+	arabicString = re.sub(ur'\u064a\b',ur'\u0649',arabicString,flags=re.U)
+	#Normalize Arabic numerals
+	for num in numerals:
+		arabicString = re.sub(num,numerals[num],arabicString,flags=re.U)
+	#Re-encode as utf-8
+	arabicString = arabicString.encode('utf-8')
 	return arabicString
 
 def main(args):
--- scripts/arabicTokenize.py
+++ scripts/arabicTokenize.py
@@ -8,13 +8,13 @@ import sys, re, fileinput
 #Compile digit and punctuation regex
 num = ur'0123456789'
 arnum = ur'\u0660\u0661\u0662\u0663\u0664\u0665\u0666\u0667\u0668\u0669\u06f0\u06f1\u06f2\u06f3\u06f4\u06f5\u06f6\u06f7\u06f8\u06f9'
-arpunc = ur'\u2039\u203a\u00ab\u00bb\u2026\u0609\u060a\u060c\u060d\u061b\u061f\u066a\u066c\u066d'
-punctok = re.compile(ur'([%s' % re.escape('!"#$%&\'()*+,-/:;<=>?@[\\]^_`{|}~') + arpunc + ur'])')
-numtokleft = re.compile(ur'([^\.\u06d4])([' + num + arnum + ']+)')
-numtokright = re.compile(ur'([' + num + arnum + ']+)([^\.\u06d4])')
-dottokleft = re.compile(ur'(\D)([\.\u06d4])')
-dottokright = re.compile(ur'([\.\u06d4])(\D)')
-finaldottok = re.compile(ur'([\.\u06d4])$')
+arpunc = ur'\u2039\u203a\u00ab\u00bb\u2026\u0609\u060a\u060c\u060d\u061b\u061e\u061f\u066a\u066b\u066c\u066d'
+punctok = re.compile(ur'([%s' % re.escape('!"#$%&\'()*+,-/:;<=>?@[\\]^_`{|}~') + arpunc + ur'])',flags=re.U)
+numtokleft = re.compile(ur'([^\.\u06d4\d])([' + num + arnum + ']+)',flags=re.U)
+numtokright = re.compile(ur'([' + num + arnum + ur']+)([^\.\u06d4\d])',flags=re.U)
+dottokleft = re.compile(ur'(\D)([\.\u06d4])',flags=re.U)
+dottokright = re.compile(ur'([\.\u06d4])(\D)',flags=re.U)
+finaldottok = re.compile(ur'([\.\u06d4])$',flags=re.U)
 
 #Tokenize Arabic string (insert spaces around numbers and punctuation)
 def tokArabic(arabicString):
