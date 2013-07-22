815b7d302d15f2f1e09e3a731bf51594e164559d
Wes <wfeely@cs.cmu.edu>
2013-07-22 14:50:27 -0400

    made tokenize post-processor robust to ellipsis

--- scripts/tokenize.sh
+++ scripts/tokenize.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-cat $1 | ./tokenizer.sed | sed -e 's/\(\.\)\([^0-9]\)/\1 \2/g' | sed -e 's/\([^0-9]\)\(\.\)/\1 \2/g' | sed -e 's/\s\+/ /g'
+cat $1 | ./tokenizer.sed | sed -e 's/\(\.+\)\([^0-9]\)/\1 \2/g' | sed -e 's/\([^0-9]\)\(\.+\)/\1 \2/g' | sed -e 's/\s\+/ /g'
