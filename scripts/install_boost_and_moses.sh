5998c3575b4f3ce6fbb5f62c3acc21fe5353650f
wfeely <wfeely@cs.cmu.edu>
2013-10-10 17:32:13 -0400

    updated installer script

--- scripts/install_boost_and_moses.sh
+++ scripts/install_boost_and_moses.sh
@@ -7,10 +7,10 @@ wget http://downloads.sourceforge.net/project/boost/boost/1.51.0/boost_1_51_0.ta
 tar -xvzf boost_1_51_0.tar.gz
 cd boost_1_51_0
 ./bootstrap.sh
-./b2 --with=all --prefix=$HOME/boost -j 5 install threading=multi
+./b2 --with=all --prefix=$HOME/usr/local -j 5 install threading=multi
 
 #Export boost root (put this in your .bashrc)
-export BOOST_ROOT=$HOME/boost
+export BOOST_ROOT=$HOME/usr/local
 
 #Move to github folder
 mkdir $HOME/github
@@ -19,7 +19,7 @@ cd $HOME/github
 #Install Moses
 git clone https://github.com/moses-smt/mosesdecoder.git
 cd mosesdecoder
-./bjam -j5 --with-boost=$HOME/boost
+./bjam -j5 --with-boost=$BOOST_ROOT
 
 cd $HOME/github
 
@@ -27,7 +27,7 @@ cd $HOME/github
 git clone https://github.com/redpony/cdec
 cd cdec
 autoreconf -ifv
-./configure && make
+./configure --prefix=$HOME/usr/local && make
 
 #Install python
 cd $HOME
