3a9aed1dfed20e4785da210c1e29f4f1c97126af
wfeely <wfeely@cs.cmu.edu>
2013-10-10 14:25:39 -0400

    added script to install boost, moses, cdec, ducttape, multeval

---
+++ scripts/install_boost_and_moses.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+cd $HOME
+
+#Install boost
+wget http://downloads.sourceforge.net/project/boost/boost/1.51.0/boost_1_51_0.tar.gz
+tar -xvzf boost_1_51_0.tar.gz
+cd boost_1_51_0
+./bootstrap.sh
+./b2 --with=all --prefix=$HOME/boost -j 5 install threading=multi
+
+#Export boost root (put this in your .bashrc)
+export BOOST_ROOT=$HOME/boost
+
+#Move to github folder
+mkdir $HOME/github
+cd $HOME/github
+
+#Install Moses
+git clone https://github.com/moses-smt/mosesdecoder.git
+cd mosesdecoder
+./bjam -j5 --with-boost=$HOME/boost
+
+cd $HOME/github
+
+#Install cdec
+git clone https://github.com/redpony/cdec
+cd cdec
+autoreconf -ifv
+./configure && make
+
+#Install python
+cd $HOME
+wget http://www.python.org/ftp/python/2.7.5/Python-2.7.5.tgz
+tar -xvzf Python-2.7.5.tgz
+cd Python-2.7.5.tgz
+./configure --prefix=$HOME/usr/local && make && make install
+
+#Export new python path (put this in your .bashrc)
+export PATH=$HOME/usr/local:$PATH
+#alias python="python2.7"
+
+#Install ducttape
+cd $HOME/github
+git clone https://github.com/jhclark/ducttape.git
+
+#Export ducttape path (put this in your .bashrc)
+export PATH=$HOME/github/ducttape:$PATH
+
+#Install multeval
+cd $HOME/github
+git clone https://github.com/jhclark/multeval
