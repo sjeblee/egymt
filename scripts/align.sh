6c6fdb2f06490c453310c7f46a32ce674054d5c9
wfeely <wfeely@cs.cmu.edu>
2013-12-01 16:32:06 -0500

    changed align script to grow-diag symmetrization, fixed bash error

--- scripts/align.sh
+++ scripts/align.sh
@@ -5,6 +5,6 @@
 
 mkdir -p tmp
 
-~/github/cdec/word-aligner/fast_align -d -v -o -i $0 > tmp/fwd_align.txt
-~/github/cdec/word-aligner/fast_align -r -d -v -o -i $0 > tmp/rev_align.txt
-~/github/cdec/utils/atools -i tmp/fwd_align.txt -j tmp/rev_align.txt -c grow-diag-final-and > align.txt
+~/github/cdec/word-aligner/fast_align -d -v -o -i $1 > tmp/fwd_align.txt
+~/github/cdec/word-aligner/fast_align -r -d -v -o -i $1 > tmp/rev_align.txt
+~/github/cdec/utils/atools -i tmp/fwd_align.txt -j tmp/rev_align.txt -c grow-diag > align.txt
