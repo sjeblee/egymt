a2d181bea1444f89062ea56e50d45e154d3851e7
Weston Feely <wfeely@workhorse.lti.cs.cmu.edu>
2014-02-04 05:12:13 -0500

    added installs for automake and autoconf

--- scripts/install_boost_moses_cdec.sh
+++ scripts/install_boost_moses_cdec.sh
@@ -1,27 +1,64 @@
 #!/bin/bash
 #install_boost_moses_cdec
 
-# Install boost
+# Define prefix (put this in your .bashrc)
+export prefix=$HOME/usr/local
+
+# Install autoconf (needed for cdec)
+cd $HOME
+git clone --depth=1 git://git.sv.gnu.org/autoconf.git
+cd autoconf
+git checkout v2.64
+autoreconf -vi
+./configure --prefix=$prefix
+make install
+
+# Install automake (needed for cdec)
+cd $HOME
+git clone git://git.sv.gnu.org/automake.git
+cd automake
+git checkout v1.11.2
+./bootstrap
+./configure --prefix=$prefix
+make install
+
+# Install gettext and gperf (needed for GNU coreutils)
+#cd $HOME
+#wget http://ftp.gnu.org/pub/gnu/gettext/gettext-0.18.3.1.tar.gz
+#tar -xzvf gettext-0.18.3.1.tar.gz
+#cd gettext-0.18.3.1
+#./configure --prefix=$prefix
+#make
+#make install
+#cd $HOME
+#wget http://ftp.gnu.org/pub/gnu/gperf/gperf-3.0.4.tar.gz
+#tar -xvzf gperf-3.0.4.tar.gz
+#cd gperf-3.0.4
+#./configure --prefix=$prefix
+#make
+#make install
+
+# Install boost (needed for moses and cdec)
 cd $HOME
 wget http://downloads.sourceforge.net/project/boost/boost/1.51.0/boost_1_51_0.tar.gz
 tar -xvzf boost_1_51_0.tar.gz
 cd boost_1_51_0
 ./bootstrap.sh
-./b2 --with=all --prefix=$HOME/usr/local -j 4 install threading=multi
+./b2 --with=all --prefix=$prefix -j 4 install threading=multi
 
 # Export boost root (put this in your .bashrc)
-export BOOST_ROOT=$HOME/usr/local
+export BOOST_ROOT=$prefix
 
-# Install python
+# Install python-2.7.5 (needed for cdec)
 cd $HOME
 wget http://www.python.org/ftp/python/2.7.5/Python-2.7.5.tgz
 tar -xvzf Python-2.7.5.tgz
 cd Python-2.7.5.tgz
-./configure --prefix=$HOME/usr/local && make && make install
+./configure --prefix=$prefix && make && make install
 
 # Export new python path (put this in your .bashrc)
-export PATH=$HOME/usr/local:$PATH
-export PYTHON=$HOME/usr/local/bin/python
+export PATH=$prefix:$prefix/bin:$PATH
+export PYTHON=$prefix/bin/python
 alias python="$HOME/usr/local/bin/python"
 
 # Make github folder if necessary
@@ -63,16 +100,16 @@ cd multeval
 ./get_deps.sh
 ant
 
-# Install pv
+# Install pv (needed for Pipelines)
 cd $HOME
 wget http://pipeviewer.googlecode.com/files/pv-1.1.4.tar.bz2
 tar xjf pv-1.1.4.tar.bz2
 cd pv-1.1.4
-./configure --prefix=$HOME/usr/local
+./configure --prefix=$prefix
 make
 make install
 
-# Install parallel
+# Install parallel (needed for Pipelines)
 wget http://git.savannah.gnu.org/cgit/parallel.git/plain/src/parallel
 chmod 755 parallel
 cp parallel sem
