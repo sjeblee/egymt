c2c2ec6e0b68ce6b161baf87a49360eb7f0a65bc
wfeely <wfeely@users.noreply.github.com>
2014-02-27 16:11:27 -0500

    changed from gd to gdfa symmetrization

--- scripts/align.sh
+++ scripts/align.sh
@@ -13,4 +13,4 @@ mkdir -p tmp
 
 ~/github/cdec/word-aligner/fast_align -d -v -o -i $1 > tmp/fwd_align.txt
 ~/github/cdec/word-aligner/fast_align -r -d -v -o -i $1 > tmp/rev_align.txt
-~/github/cdec/utils/atools -i tmp/fwd_align.txt -j tmp/rev_align.txt -c grow-diag > align.txt
+~/github/cdec/utils/atools -i tmp/fwd_align.txt -j tmp/rev_align.txt -c grow-diag-final-and > align.txt
