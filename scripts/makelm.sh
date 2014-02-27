4815fa32624184234bea4090f4d98e58f5dc9aa7
wfeely <wfeely@cs.cmu.edu>
2014-02-27 16:27:09 -0500

    added script to make lm using lmplz

---
+++ scripts/makelm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#makelm.sh
+#Weston Feely
+#2/27/14
+
+#Check for required arg1
+if [ -z "$1" ]; then
+	echo "Usage: ./makelm.sh data.txt"
+	exit 1
+fi
+
+tmp="./tmp"
+percent="10%"
+data=$1
+arpafile="./lm.arpa"
+lm_quant_flags="-q 8 -b 8 -a 22"
+binfile="./lm.bin"
+
+mkdir -p $tmp
+
+~/github/mosesdecoder/bin/lmplz -o $order -T $tmp -S $percent < $data > $arpafile
+~/github/mosesdecoder/bin/build_binary $lm_quant_flags trie $arpafile $binfile
